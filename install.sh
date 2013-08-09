59f845816b9d592199c072212f6e3b88b6ad48f4
Jose Espinosa <jhespinosa@gmail.com>
2013-08-09 08:45:33 -0700

    Set oh-my-zsh to track origin

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
-#!/bin/sh -ex
+#!/bin/sh -x
 
 # First we install oh-my-zshell if not available
 if [ ! -d ${HOME}/.oh-my-zsh ]; then
   curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+  cd ${HOME}/.oh-my-zsh
   git remote rm origin
   git remote add origin git@github.com:jose-espinosa/oh-my-zsh.git
   git remote add upstream git://github.com/robbyrussell/oh-my-zsh.git
+  git fetch origin
+  git branch --set-upstream-to=origin/master master
+  git pull origin master
 fi
 
 git clone git@github.com:jose-espinosa/dotfiles.git ${HOME}/.dotfiles

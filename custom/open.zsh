fefb9742aaf38832589f3dedcdf5410500780d40
Jose Espinosa <jhespinosa@gmail.com>
2024-06-13 14:19:29 -0700

    make the o alias more multiplataform

--- custom/open.zsh
+++ custom/open.zsh
@@ -1,10 +1,20 @@
 
 o () {
-  command=/usr/bin/xdg-open;
+  case $XDG_CURRENT_DESKTOP in
+    GMOME)
+      command=/usr/bin/xdg-open ;;
+    KDE)
+      command=/usr/bin/open ;;
+    *)
+      command=/dev/null ;;
+  esac
+
   if [ -x $command ]; then
     for i in $@; do
       $command $i
     done
+  else
+    echo "$basename: no open command find"
   fi
 }
 
